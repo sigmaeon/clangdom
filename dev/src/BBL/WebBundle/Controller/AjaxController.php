758d3ecfbef200124d825b09ed4d03887c35ee74
simeon <clangdom@gmail.com>
2013-04-06 01:49:37 +0200

    popup works

--- dev/src/BBL/WebBundle/Controller/AjaxController.php
+++ dev/src/BBL/WebBundle/Controller/AjaxController.php
@@ -13,7 +13,16 @@ class AjaxController extends Controller
     
     public function loadAction()
     {
+    	$objects['ob1']['picture'] = "oma whaaaa";
+    	$objects['ob1']['link'] = "here is a link to oma";
+    	$objects['ob1']['info'] = "hell yeah this is oma";
+    	$objects['ob1']['name'] = "OMA";
+    	
+    	$objects['ob2']['picture'] = "koma whaaaa";
+    	$objects['ob2']['link'] = "here is a link to koma";
+    	$objects['ob2']['info'] = "hell yeah this is koma";
+    	$objects['ob2']['name'] = "KOMA";
     	return $this->render('BBLWebBundle:Base:content.html.twig', 
-    							array('name' => "Oma", 'title' => "the Oma show", 'info' => "Hey this is a oma"));
+    							array('objects' => $objects, 'title' => "A band of Omas"));
     }
 }
