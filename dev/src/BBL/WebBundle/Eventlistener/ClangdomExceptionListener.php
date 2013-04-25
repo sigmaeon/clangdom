346efeb52f66b5f2ad1029a661acb6c4fafde31f
simeon <clangdom@gmail.com>
2013-04-25 14:52:37 +0200

    added ExceptionListener

---
+++ dev/src/BBL/WebBundle/Eventlistener/ClangdomExceptionListener.php
@@ -0,0 +1,11 @@
+<?php
+namespace BBL\WebBundle\Eventlistener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ClangdomExceptionListener
+{
+	
+}
